473e9a3cebe510031cd5c8e3075505a7496ef0cd
Zychaowill <15536542139@163.com>
2018-06-02 19:19:31 +0800

    remove MapHelper

--- src/main/java/org/streamframework/foundation/collections/ListHelper.java
+++ src/main/java/org/streamframework/foundation/collections/ListHelper.java
@@ -1,5 +1,31 @@
 package org.streamframework.foundation.collections;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.*;
+
 public class ListHelper {
 
+    public <T> Map<T, Boolean> transform(List<T> list) {
+        if (CollectionUtils.isEmpty(list)) {
+            return new HashMap<>(0);
+        }
+        Boolean empty = Boolean.TRUE;
+        Map<T, Boolean> targets = new HashMap<>(list.size());
+        for (T t : list) {
+            targets.put(t, empty);
+        }
+        return targets;
+    }
+
+    public <T> Map<T, Boolean> transform(T... args) {
+        if (ArrayUtils.isEmpty(args)) {
+            return new HashMap<>(0);
+        }
+        Boolean empty = Boolean.TRUE;
+        return Arrays.stream(args)
+                .filter(Objects::nonNull)
+                .collect(HashMap::new, (m, v) -> m.put(v, empty), HashMap::putAll);
+    }
 }
